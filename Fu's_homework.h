fb521ed4c559d57c3853ef2d4c865c74b4ab5b2b
wuhan <340473925@qq.com>
2022-09-02 18:21:55 +0800

    ..

---
+++ Fu's_homework.h
@@ -0,0 +1,88 @@
+#pragma once
+
+ // 《数字几何处理》傅孝明课程，作业列表
+/*
+1. 基本练习Basic training
+   最短路径，三角网格的最小生成树；
+     Reference: Shortest path (Dijkstra’s algorithm) and minimal spanning tree on the triangular mesh
+   Deadline: 23:59 2020/3/14
+
+
+2. 曲率计算和可视化；Curvature estimation and visualization
+   Deadline: 23:59 2020/3/21
+
+
+3. 网格平滑Mesh Smoothing
+   使用双边法向滤波实现网格去噪；
+   Reference: Bilateral Normal Filtering for Mesh Denoising
+   Deadline: 23:59 2020/3/28
+
+
+4. 网格参数化 Mesh Parameterization 1
+   tutte嵌入（重心嵌入）
+   Reference: Tutte's embedding method
+   Deadline: 23:59 2020/4/4
+
+
+5. 网格参数化 Mesh Parameterization 2
+   网格参数化的局部/全局方法
+   Reference: A Local/Global Approach to Mesh Parameterization
+   Deadline: 23:59 2020/4/11
+
+
+6. 网格变形 Mesh Deformation
+   尽可能刚性的曲面建模
+   Reference: As-Rigid-As-Possible Surface Modeling
+   Deadline: 23:59 2020/4/18
+
+
+7. 重心坐标 Barycentric Coordinates
+   均值坐标
+   Reference: Mean value coordinates
+   Deadline: 23:59 2020/4/25
+
+
+8. 网格精简Mesh Simplification
+   基于二次误差度量的网格精简
+   Reference: Surface Simplification Using Quadric Error Metrics
+   Deadline: 23:59 2020/5/2
+
+
+9. 网格插值 Mesh Interpolation
+   尽可能刚性形状差值
+   Reference: As-Rigid-As-Possible Shape Interpolation
+   Deadline: 23:59 2020/5/9
+
+
+10. lloyd迭代算法 Lloyd’s iteration algorithm
+    Reference: Variational shape approximation
+    Deadline: 23:59 2020/5/16
+
+
+11. 重划分 Remeshing
+    多分辨率建模中的重划分方法
+    Reference: A Remeshing Approach to Multiresolution Modeling
+    Deadline: 23:59 2020/5/23
+
+
+12. 最佳德劳内三角剖分 Optimal Delaunay triangulation
+    最佳德劳内三角剖分
+    Reference: Optimal Delaunay triangulation
+    Deadline: 23:59 2020/5/30
+
+
+13. 交叉场 Cross Fields
+    ？？？
+    Reference: Designing N-polyvector fields with complex polynomials
+    Deadline: 23:59 2020/6/13
+
+
+14. 几何最优化 Geometric Optimization
+    ？？？
+    Reference: Scalable Locally Injective Mappings
+    Deadline: 23:59 2020/6/27
+
+
+
+
+*/
---
+++ HW1_MST_dijkstra.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+// HW1——三角网格上实现dijstra算法和最小支撑树生成算法；
+
+namespace FUS_HW 
+{
+
+
+
+}
